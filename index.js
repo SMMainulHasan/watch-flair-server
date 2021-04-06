8c70b60bf97d0a9886189cc04367dd60e63d2ff2
SMMainulHasan <mainulhasan7070@gmail.com>
2021-04-06 19:05:02 +0600

    checkout and ordered list api created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 require('dotenv').config();
 const port = 5055;
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cxq2v.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -17,23 +18,52 @@ app.get('/', (req, res) => {
 
 client.connect(err => {
     const productCollection = client.db("watchFlair").collection("products");
+    const orderedProductCollection = client.db("watchFlair").collection("orderedProduct");
 
     app.post('/addProduct', (req, res) => {
         productCollection.insertOne(req.body)
             .then(result => {
-                if(result.insertedCount > 0){
+                if (result.insertedCount > 0) {
                     res.status(200).send('Product added successfully!')
                 }
-                else{
+                else {
                     res.status(401).send('Server side error!')
                 }
             })
     })
 
+    app.get('/product-detail/:id', (req, res) => {
+        const id = req.params.id;
+        productCollection.find({ _id: ObjectID(id) })
+            .toArray((err, document) => {
+                res.send(document[0])
+            })
+    })
+
     app.get('/product', (req, res) => {
         productCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.post('/addOrderPlace', (req, res) => {
+        orderedProductCollection.insertOne(req.body)
+            .then(result => {
+                if (result.insertedCount > 0) {
+                    res.status(200).send('Product added successfully!')
+                }
+                else {
+                    res.status(401).send('Server side error!')
+                }
+            })
+    })
+
+
+    app.get('/orders/:email', (req, res) => {
+        orderedProductCollection.find({ email: req.params.email })
+            .toArray((err, documents)=> {
+            res.send(documents)
         })
     })
 
