2fef1d6129d9e5b9156e803531c70e0d91e4106d
SMMainulHasan <mainulhasan7070@gmail.com>
2021-04-07 02:49:45 +0600

    should work

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 app.use(cors());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-  }) 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  }); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
