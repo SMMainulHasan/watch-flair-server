08731cb0558e680286f85a6ae489cfabb568d106
SMMainulHasan <mainulhasan7070@gmail.com>
2021-04-07 12:19:23 +0600

    cors fixed

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  }); 
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   }); 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
